a0df009e4f6ecf9f7c8b6045cff2687aeec94c88
DreamLineLove <zwenyanzaw@protonmail.com>
2023-05-31 15:20:42 +0630

    Minor changes to handlingNil.go

---
+++ cmd/pointers/handlingNil.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func handlingNil() {
+	var nilBooleanPointer *bool
+
+	changeBooleanValue(nilBooleanPointer)
+
+	var trueBool bool = true
+	WriteSpace()
+	fmt.Println("Original value:", trueBool)
+
+	nilBooleanPointer = &trueBool
+	changeBooleanValue(nilBooleanPointer)
+
+	WriteSpace()
+	fmt.Println("Changed value:", trueBool)
+
+	NewLine()
+	WriteSpace()
+	fmt.Println("-\tRead the source code to learn more")
+}
+
+func changeBooleanValue(boolean *bool) {
+	// if the Pointer boolean happens to be nil, the function will exit with a standard output
+	if boolean == nil {
+		WriteSpace()
+		fmt.Println("ERROR occurred when trying to access the pointer")
+		return
+	}
+
+	// if not it will perform the intended logic
+	*boolean = !*boolean
+}
