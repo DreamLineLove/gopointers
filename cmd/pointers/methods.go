5b85351fbd870088683bb0051fa1c1c4eb335957
DreamLineLove <zwenyanzaw@protonmail.com>
2023-05-31 11:39:25 +0630

    Furthered methods()

--- cmd/pointers/methods.go
+++ cmd/pointers/methods.go
@@ -1,9 +1,26 @@
 package main
 
+import "fmt"
+
 type GitRemoteRepository struct {
 	ProviderName string
 }
 
-func methods() {
+func methods(methodsString string) {
+	gh := GitRemoteRepository{
+		ProviderName: methodsString,
+	}
 
+	WriteSpace()
+	fmt.Println("Original value:", methodsString)
+
+	var newValue string
+
+	WriteSpace()
+	fmt.Print("Provide new value: ")
+	_, err := fmt.Scan(&newValue)
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
 }
