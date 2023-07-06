f3b1cb345a4faa50b6955a81b0dbd5a2b97577c7
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-06 21:18:41 +0630

    constrast between pointer functions and no-pointer functions

--- cmd/pointers/revisiting.go
+++ cmd/pointers/revisiting.go
@@ -7,12 +7,25 @@ func revisiting() {
 	int1 = 55
 	var ptr1 *int32
 	ptr1 = &int1
-
 	fmt.Println("original value: ", int1)
+
+	// Note that both work!
 	changePtrByDereferencing(ptr1, 66)
 	fmt.Println("new value 1: ", int1)
+	changePtrByDereferencing(&int1, 99)
+	fmt.Println("new value 2: ", int1)
+	passingValues(int1, 100)
+	fmt.Println("new value 3 (expect 99): ", int1)
 }
 
 func changePtrByDereferencing(intPtr *int32, newValue int32) {
 	*intPtr = newValue
+}
+
+func passingValues(integer int32, newValue int32) {
+	fmt.Println("original value when passed: ", integer)
+
+	// this should only effect the function's local (parameter) copy of the value passed in
+	integer = newValue
+	fmt.Println("new value: ", integer)
 }
